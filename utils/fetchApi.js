531bc17a01c9bd8bdc4022ce0291030ca46d2d63
Pep Nieto <pepnieto97@gmail.com>
2022-05-29 12:33:54 +0200

    starting the project

---
+++ utils/fetchApi.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+export const baseUrl = 'https://bayut.p.rapidapi.com'
+
+export const fetchApi = async (url) => {
+    const {data} = await axios.get((url,{}),{
+        headers: {
+            'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
+            'X-RapidAPI-Key': '2dc16a99ccmshfab78b67b936d94p1ef7eejsnb19ab9532d58'
+          }
+    })
+    
+return data;
+}
+
